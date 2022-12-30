caba40f15bd4faed3cf86f00c77750efd3267ea5
defan312 <106823339+defan312@users.noreply.github.com>
2022-12-30 10:35:41 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×' 
+global.apikey = ["ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×", "ᴬᴺᴵᴬ ᴮᴼᵀ", "ᴬᴺᴵᴬ ᴮᴼᵀ"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
