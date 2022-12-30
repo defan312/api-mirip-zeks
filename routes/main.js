e730bbf7f7847e327a49be2f319cdc7264c7cc66
defan312 <106823339+defan312@users.noreply.github.com>
2022-12-30 10:38:26 +0700

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -35,8 +35,8 @@ var port = process.env.PORT || 8080 || 5000 || 3000
         uptime: muptime(process.uptime()),
         speed: `${neww - old}ms`,
         info:{       
-            owner: 'ZeeoneOfc',            
-            apikey: 'Chat Owner: https://wa.me/62887435047326'
+            owner: 'ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×',            
+            apikey: 'Chat Owner: https://wa.me/6281321776496'
         }
     }
     res.json(status)
